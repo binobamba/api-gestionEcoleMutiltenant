e5e1b39d7abae256bc02966cd28068da4dfa5705
bino.bamba <bambafulgence622@gmail.com>
2026-03-02 17:21:34 +0000

    mise à jour des données

---
+++ src/test/java/com/edusecure/SchoolManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edusecure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
